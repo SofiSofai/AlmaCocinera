28fb2892ac194761fca4f543cd84d8593eb4640a
DobleA74 <andresandriani@gmail.com>
2023-11-08 21:56:36 -0300

    smtp gmail y confirmation.html

--- contact.php
+++ contact.php
@@ -1,31 +1,30 @@
 <?php
+// Verifica si la solicitud es de tipo POST (cuando el formulario se envía)
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $nombre = $_POST["nombre"];
-  $email = $_POST["email"];
-  $mensaje = $_POST["mensaje"];
+    // Recupera los datos del formulario
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["text"];
 
-  $to = "info@estacionkm325.com.ar"; // Reemplaza con tu dirección de correo electrónico
-  $subject = "Nuevo mensaje de contacto de $nombre";
-  $message = "Nombre: $nombre\n";
-  $message .= "Correo Electrónico: $email\n";
-  $message .= "Mensaje:\n$mensaje";
+    // Configuración del correo electrónico
+    $to = "yekacomidas@gmail.com"; // Reemplaza con tu dirección de correo electrónico
+    $subject = "Nuevo mensaje de contacto de $name";
+    $message = "Nombre: $name\nCorreo Electrónico: $email\nMensaje:\n$message";
+    $headers = "From: $email";
 
-  $headers = "From: $email";
+    // Configuración del servidor SMTP de Gmail
+    ini_set("SMTP", "smtp.gmail.com");
+    ini_set("smtp_port", "587");
+    ini_set("sendmail_from", "yekacomidas@gmail.com"); // Reemplaza con tu dirección de correo electrónico de Gmail
 
-  // Configura el servidor SMTP
-  ini_set("SMTP", "mail.estacionkm325.com.ar");
-  ini_set("smtp_port", 465);
+    // Autenticación de Gmail
+    $username = "yekacomidas@gmail.com"; // Reemplaza con tu dirección de correo electrónico de Gmail
+    $password = "acomerla"; // Reemplaza con la contraseña de tu cuenta de Gmail
 
-  // Configura la autenticación
-  ini_set("sendmail_from", "info@estacionkm325.com.ar");
-  ini_set("auth_username", "info@estacionkm325.com.ar");
-  ini_set("auth_password", "infoestacionkm325");
+    // Envía el correo electrónico utilizando SMTP de Gmail
+    mail($to, $subject, $message, $headers, "-f$yekacomidas@gmail.com");
 
-  // Envía el correo electrónico
-  mail($to, $subject, $message, $headers);
-
-  // Redirige al usuario a una página de confirmación
-  header("Location: confirmation.html"); // Crea una página de confirmación si no la tienes
+    // Redirige al usuario a una página de confirmación
+    header("Location: confirmation.html"); // Crea una página de confirmación si no la tienes
 }
 ?>
-
