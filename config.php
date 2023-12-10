344244cb13b34648bcd1230f5100197ad435e913
DobleA74 <andresandriani@gmail.com>
2023-12-10 13:20:47 -0300

    credenciales

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 // Configuración para el servidor SMTP
-define('SMTP_HOST', 'mail.estacionkm325.com.ar');
+define('SMTP_HOST', 'c1921745.ferozo.com');
 define('SMTP_PORT', 465);
-define('SMTP_USERNAME', 'info@estacionkm325.com.ar');
-define('SMTP_PASSWORD', 'infoestacionkm325');
+define('SMTP_USERNAME', 'contacto@almacocinera.com');
+define('SMTP_PASSWORD', 'GV*3mZb8bM');
 
 ?>
